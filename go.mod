9acf2b19f375ca74eb5010521b2443b5355394a4
Jean de Klerk <deklerk@google.com>
2019-02-27 15:38:09 -0700

    internal: improve CI/CD scripts
    - Use go mod tidy to check if deps were added without being added to go.mod.
    - Use go mod download to download deps instead of go get.
    - Use GO111MODULE=on everywhere applicable.
    - Switch to being 1.12 aware.
    - Refactor dependency downloading into function to be consistent with
    other repos (and more readable.

--- go.mod
+++ go.mod
@@ -1,11 +1,9 @@
 module github.com/googleapis/google-cloud-go-testing
 
 require (
-	cloud.google.com/go v0.34.0
-	github.com/google/martian v2.1.0+incompatible // indirect
-	github.com/googleapis/gax-go v2.0.2+incompatible // indirect
-	golang.org/x/lint v0.0.0-20181026193005-c67002cb31c3
-	golang.org/x/tools v0.0.0-20180828015842-6cd1fcedba52
+	cloud.google.com/go v0.36.0
+	golang.org/x/lint v0.0.0-20190301231843-5614ed5bae6f
+	golang.org/x/tools v0.0.0-20190305160728-f8c04913dfb7
 	google.golang.org/api v0.1.0
-	honnef.co/go/tools v0.0.0-20180728063816-88497007e858
+	honnef.co/go/tools v0.0.0-20190215041234-466a0476246c
 )
--- internal/kokoro/test.sh
+++ internal/kokoro/test.sh
@@ -15,7 +15,7 @@ go version
 
 # Set $GOPATH
 export GOPATH="$HOME/go"
-export GCGT_HOME=$GOPATH/src/googleapis/google-cloud-go-testing
+export GCGT_HOME=$GOPATH/src/github.com/googleapis/google-cloud-go-testing
 export PATH="$GOPATH/bin:$PATH"
 mkdir -p $GCGT_HOME
 
@@ -24,8 +24,20 @@ git clone . $GCGT_HOME
 cd $GCGT_HOME
 
 try3() { eval "$*" || eval "$*" || eval "$*"; }
-try3 go get -v -t ./...
 
+download_deps() {
+    if [[ `go version` == *"go1.11"* ]] || [[ `go version` == *"go1.12"* ]]; then
+        export GO111MODULE=on
+        # All packages, including +build tools, are fetched.
+        try3 go mod download
+    else
+        # Because we don't provide -tags tools, the +build tools
+        # dependencies aren't fetched.
+        try3 go get -v -t ./...
+    fi
+}
+
+download_deps
 ./internal/kokoro/vet.sh
 
 # Run tests and tee output to log file, to be pushed to GCS as artifact.
